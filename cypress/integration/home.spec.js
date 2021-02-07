f7e046d9405e5b3aff1729e6200d88a1d7eeb539
MichaelX <vinoth.michaelxavier@gmail.com>
2021-02-07 11:44:25 -0500

    auth tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,33 +1,61 @@
 describe('Route: Home', () => {
 
-  it('adds document to firestore collection', () => {
-    cy.callFirestore('add', 'this is a test', { some: 'value' });
+  describe('Unauthenticated', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+    });
+
+    it('Redirects to Sign-In route when unauthenticated', () => {
+      cy.url().should('include', '/sign-in');
+      cy.contains('Sign In with Google');
+    });
+
+    it('Redirects to Workflow route on sign-in', () => {
+      cy.login();
+      cy.url().should('include', '/workflow');
+    });
+
+    afterEach(() => {
+      cy.logout();
+    });
   });
 
-  // beforeEach(() => {
-  //   cy.visit('http://localhost:3000');
-  // });
+  describe('Authenticated', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+      cy.login();
+    });
 
-  // it('Redirects to Workflow route', () => {  
-  //   cy.url().should('include', '/workflow');
-  // });
+    afterEach(() => {
+      cy.logout();
+    });
 
-  // it('can navigate to Projects route', () => {
-  //   cy.get('[data-testid="SVGSideBar"]').click();
-  //   cy.get('[data-testid="projects"]').click();
-  //   cy.url().should('include', '/projects');
-  // });
+    it('Redirects to Workflow route on sign-in', () => {
+      cy.visit('http://localhost:3000');
+      cy.login();
+      cy.url().should('include', '/workflow');
+    });
 
-  // it('can navigate to Users route', () => {
-  //   cy.get('[data-testid="SVGSideBar"]').click();
-  //   cy.get('[data-testid="users"]').click();
-  //   cy.url().should('include', '/users');
-  // });
+    it('can navigate to Projects route', () => {
+      cy.get('[data-testid="SVGSideBar"]').click();
+      cy.get('[data-testid="projects"]').click();
+      cy.url().should('include', '/projects');
+    });
 
-  // it('can navigate to Account route', () => {
-  //   // cy.get('[data-testid="SVGSideBar"]').click();
-  //   cy.get('[data-testid="account"]').click();
-  //   cy.url().should('include', '/account');
-  // });
+    it('can navigate to Users route', () => {
+      cy.get('[data-testid="SVGSideBar"]').click();
+      cy.get('[data-testid="users"]').click();
+      cy.url().should('include', '/users');
+    });
 
-})
+    it('can navigate to Account route', () => {
+      cy.get('[data-testid="account"]').click();
+      cy.url().should('include', '/account');
+    });
+  });
+});
+
+// Firestore Tests
+// it('adds document to firestore collection', () => {
+//   cy.callFirestore('add', 'this is a test', { some: 'value' });
+// });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,13 +31,13 @@ import 'firebase/firestore';
 import { attachCustomCommands } from 'cypress-firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: Cypress.env('apiKey'),
+  authDomain: Cypress.env('authDomain'),
+  projectId: Cypress.env('projectId'),
+  storageBucket: Cypress.env('storageBucket'),
+  messagingSenderId: Cypress.env('messagingSenderId'),
+  appId: Cypress.env('appId'),
+  measurementId: Cypress.env('measurementId'),
 }
 
 firebase.initializeApp(firebaseConfig);
