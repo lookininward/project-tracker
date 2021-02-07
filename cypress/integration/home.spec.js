687422e57c08e0023168c869b3bccb72ba68aed7
MichaelX <vinoth.michaelxavier@gmail.com>
2021-02-07 10:49:09 -0500

    add cypress-firebase; config; firestore

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,29 +1,33 @@
 describe('Route: Home', () => {
 
-  beforeEach(() => {
-    cy.visit('http://localhost:3000');
+  it('adds document to firestore collection', () => {
+    cy.callFirestore('add', 'this is a test', { some: 'value' });
   });
 
-  it('Redirects to Workflow route', () => {  
-    cy.url().should('include', '/workflow');
-  });
+  // beforeEach(() => {
+  //   cy.visit('http://localhost:3000');
+  // });
 
-  it('can navigate to Projects route', () => {
-    cy.get('[data-testid="SVGSideBar"]').click();
-    cy.get('[data-testid="projects"]').click();
-    cy.url().should('include', '/projects');
-  });
+  // it('Redirects to Workflow route', () => {  
+  //   cy.url().should('include', '/workflow');
+  // });
 
-  it('can navigate to Users route', () => {
-    cy.get('[data-testid="SVGSideBar"]').click();
-    cy.get('[data-testid="users"]').click();
-    cy.url().should('include', '/users');
-  });
+  // it('can navigate to Projects route', () => {
+  //   cy.get('[data-testid="SVGSideBar"]').click();
+  //   cy.get('[data-testid="projects"]').click();
+  //   cy.url().should('include', '/projects');
+  // });
 
-  it('can navigate to Account route', () => {
-    // cy.get('[data-testid="SVGSideBar"]').click();
-    cy.get('[data-testid="account"]').click();
-    cy.url().should('include', '/account');
-  });
+  // it('can navigate to Users route', () => {
+  //   cy.get('[data-testid="SVGSideBar"]').click();
+  //   cy.get('[data-testid="users"]').click();
+  //   cy.url().should('include', '/users');
+  // });
+
+  // it('can navigate to Account route', () => {
+  //   // cy.get('[data-testid="SVGSideBar"]').click();
+  //   cy.get('[data-testid="account"]').click();
+  //   cy.url().should('include', '/account');
+  // });
 
 })
