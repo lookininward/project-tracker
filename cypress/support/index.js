92faa07920db6fa29a797cd3dbfa8d63d12072e6
MichaelX <vinoth.michaelxavier@gmail.com>
2021-02-08 08:13:35 -0500

    allow errors

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -21,9 +21,9 @@ console.log('support/index');
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  console.log('Error in uncaught exception:', err.message)
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-})
+// Cypress.on('uncaught:exception', (err, runnable) => {
+//   console.log('Error in uncaught exception:', err.message)
+//   // returning false here prevents Cypress from
+//   // failing the test
+//   return false
+// })
