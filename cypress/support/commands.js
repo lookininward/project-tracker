0d7bae9b5f0d010e0376f040f6b951158a682087
MichaelX <vinoth.michaelxavier@gmail.com>
2021-02-10 16:59:35 -0500

    all vars

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,7 +30,18 @@ import 'firebase/database';
 import 'firebase/firestore';
 import { attachCustomCommands } from 'cypress-firebase';
 
-console.log('FIREBASE_CONFIG', Cypress.env('FIREBASE_CONFIG'));
+const firebaseConfig = {
+  "apiKey": Cypress.env('FIREBASE_API_KEY'),
+  "authDomain": Cypress.env('FIREBASE_AUTH_DOMAIN'),
+  "projectId": Cypress.env('FIREBASE_PROJECT_ID'),
+  "storageBucket": Cypress.env('FIREBASE_STORAGE_BUCKET'),
+  "messagingSenderId": Cypress.env('FIREBASE_MESSAGING_SENDER_ID'),
+  "appId": Cypress.env('FIREBASE_APP_ID'),
+  "measurementId": Cypress.env('FIREBASE_MEASUREMENT_ID')
+}
 
-firebase.initializeApp(Cypress.env('FIREBASE_CONFIG'));
+console.log('FIREBASE_CONFIG', Cypress.env('FIREBASE_CONFIG'));
+console.log('firebaseConfig', firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
 attachCustomCommands({ Cypress, cy, firebase });
