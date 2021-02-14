c39e2bb453a6cba57568a2f5cd0eb3bcf650cfe0
MichaelX <vinoth.michaelxavier@gmail.com>
2021-02-14 13:08:09 -0500

    update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,13 +5,13 @@ import 'firebase/firestore';
 import { attachCustomCommands } from 'cypress-firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: Cypress.env('FIREBASE_API_KEY'),
+  authDomain: Cypress.env('FIREBASE_AUTH_DOMAIN'),
+  projectId: Cypress.env('FIREBASE_PROJECT_ID'),
+  storageBucket: Cypress.env('FIREBASE_STORAGE_BUCKET'),
+  messagingSenderId: Cypress.env('FIREBASE_MESSAGING_SENDER_ID'),
+  appId: Cypress.env('FIREBASE_APP_ID'),
+  measurementId: Cypress.env('FIREBASE_MEASUREMENT_ID')
 }
 
 firebase.initializeApp(firebaseConfig);
